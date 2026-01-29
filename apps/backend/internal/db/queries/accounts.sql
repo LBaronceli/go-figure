45968535aa31624fa2b8bd1ed92e9c740993c690
Luis Baronceli <luisbaronceli@gmail.com>
2026-01-29 15:54:36 +1300

    sqlc added for accounts

---
+++ apps/backend/internal/db/queries/accounts.sql
@@ -0,0 +1,57 @@
+-- name: CreateAccount :one
+INSERT INTO accounts (
+    name,
+    type,
+    currency
+) VALUES (
+    $1, $2, $3
+)
+RETURNING
+    id,
+    name,
+    type,
+    currency,
+    created_at,
+    updated_at;
+
+-- name: GetAccount :one
+SELECT 
+  id,
+  name,
+  type,
+  currency,
+  created_at,
+  updated_at
+FROM accounts
+WHERE id = $1;
+
+-- name: ListACcounts :many
+SELECT 
+  id,
+  name,
+  type,
+  currency,
+  created_at,
+  updated_at
+FROM accounts
+ORDER BY created_at DESC;
+
+-- name: UpdateAccount :one
+UPDATE accounts
+SET
+    name = $2,
+    type = $3,
+    currency = $4
+WHERE id = $1
+RETURNING
+    id,
+    name,
+    type,
+    currency,
+    created_at,
+    updated_at;
+
+-- name: DeleteAccount :exec
+DELETE FROM accounts
+WHERE id = $1;
+
